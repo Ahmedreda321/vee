4710d8cf0d29aaea9aa08ffc1348502d14a75961
ahmedreda321 <ahmedredahmedd@gmail.com>
2025-07-06 22:38:35 +0300

    refactor: Remove unused imports and clean up DriverHomeCubit and OnBoardingWelcome files

--- lib/features/shared/onboarding/presentation/widgets/on_boarding_welcome.dart
+++ lib/features/shared/onboarding/presentation/widgets/on_boarding_welcome.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../../core/constants/app_constants.dart';
 import '../../../../../core/constants/strings_constants.dart';
